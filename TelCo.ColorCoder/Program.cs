0f3cd623de2720dcae5486cac1c3e07741f2f20b
anjalimnnit <41858612+anjalimnnit@users.noreply.github.com>
2020-08-26 07:14:43 -0700

    Update Program.cs

--- TelCo.ColorCoder/Program.cs
+++ TelCo.ColorCoder/Program.cs
@@ -18,10 +18,7 @@ namespace TelCo.ColorCoder
         {
             internal Color majorColor;
             internal Color minorColor;
-            public override string ToString()
-            {
-                return string.Format("MajorColor:{0}, MinorColor:{1}", majorColor.Name, minorColor.Name);
-            }
+           
         }
         
         /// <summary>
