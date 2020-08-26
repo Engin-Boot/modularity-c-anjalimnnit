59e3b23b516ab45dca30e8a44a847744ed30f76b
anjalimnnit <41858612+anjalimnnit@users.noreply.github.com>
2020-08-26 07:13:54 -0700

    Create CodeManual.cs

---
+++ TelCo.ColorCoder/CodeManual.cs
@@ -0,0 +1,20 @@
+
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Text;
+
+
+namespace TelCo.ColorCoder
+{
+    /// <summary>
+    /// This class provides the color code manual
+    /// </summary>
+    class CodeManual
+    {
+        public override string ToString() //overrides the ToString() in ColorLists
+        {
+            
+        }
+    }
+}
